4d58ca1b4afd87076fd0b0b75f154fc2faf53306
siddharthjn <siddharth.smu@gmail.com>
2016-06-26 17:56:20 -0300

    Basic layout of site is ready

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
   /**
    * You must include the dependency on 'ngMaterial'
    */
-  angular.module('nsawApp', ['ngMaterial'])
+  app = angular.module('nsawApp', ['ngMaterial']);
   console.log('app ready');
+
+  app.controller('openSideNav', function($scope, $mdSidenav) {
+    $scope.openLeftMenu = function() {
+      $mdSidenav('left').toggle();
+    };
+  });

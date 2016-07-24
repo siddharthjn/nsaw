87b5b8fcb54c65756e7d178df66b84a2f978a708
siddharthjn <siddharth.smu@gmail.com>
2016-07-23 22:18:26 -0300

    Sidenav closing on selecting menu

--- app.js
+++ app.js
@@ -4,17 +4,28 @@
   app = angular.module('nsawApp', ['ngMaterial']);
   console.log('app ready');
 
+  /*Controller for the menu button to open the nav bar*/
   app.controller('openSideNav', function($scope, $mdSidenav) {
     $scope.openLeftMenu = function() {
       $mdSidenav('left').toggle();
     };
   });
 
-  app.controller('StoreController', function(){
+  /*app.controller('closeNav', function($scope, $mdSidenav){
+    $scope.closeOnClick = function(){
+      $mdSidenav('left').close();
+    }
+  })*/
+
+  app.controller('StoreController', function($scope, $mdMedia, $mdSidenav){
 
     this.tab =1;
     this.setTab = function(newValue)
     {
+      if ($mdMedia('xs') || $mdMedia('sm'))
+      {
+        $mdSidenav('left').close();
+      }
       this.tab = newValue;
       if(this.tab == 1)
       {

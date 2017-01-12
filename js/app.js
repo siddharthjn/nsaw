328f848e03ef5fb6d7ff965bca84b90aa53b87eb
siddharthjn <siddharth.smu@gmail.com>
2017-01-12 07:25:36 -0400

    Modularizing code

--- js/app.js
+++ js/app.js
@@ -22,64 +22,63 @@
         $mdSidenav('left').close();
       }
       this.tab = newValue;
+      var physCol, chemCol, bioCol, plasCol, abtUs, contUs, careers;
+      /*$scope.physCol = {"background-color" : "white"};
+      $scope.chemCol = {"background-color" : "white"};
+      $scope.bioCol = {"background-color" : "white"};
+      $scope.plasCol = {"background-color" : "white"};
+      $scope.abtUs = {"background-color" : "white"};
+      $scope.contUs = {"background-color" : "white"};
+      $scope.careers = {"background-color" : "white"};*/
+
+      var button;
+
+      function changeColorButton(button){
+        $scope.button = {"background-color" : "rgb(95, 218, 204)"};
+      }
+
+      function changeButtonToWhite(button){
+        $scope.button = {"background-color" : "white"};
+      }
+
       if(this.tab == 1)
       {
         this.showAboutUs = false;
         this.products = physics;
-        $scope.physCol = {"background-color" : "rgb(95, 218, 204)"};
-        $scope.chemCol = {"background-color" : "white"};
-        $scope.bioCol = {"background-color" : "white"};
-        $scope.plasCol = {"background-color" : "white"};
-        $scope.abtUs = {"background-color" : "white"};
-        $scope.contUs = {"background-color" : "white"};
-        $scope.careers = {"background-color" : "white"};
+        changeButtonToWhite(button);
+        button = physCol;
+        changeColorButton(button);
       }
       else if(this.tab == 2)
       {
         this.showAboutUs = false;
         this.products = chemistry;
-        $scope.physCol = {"background-color" : "white"};
-        $scope.chemCol = {"background-color" : "rgb(95, 218, 204)"};
-        $scope.bioCol = {"background-color" : "white"};
-        $scope.plasCol = {"background-color" : "white"};
-        $scope.abtUs = {"background-color" : "white"};
-        $scope.contUs = {"background-color" : "white"};
-        $scope.careers = {"background-color" : "white"};
+        changeButtonToWhite(button);
+        button = chemCol;
+        changeColorButton(button);
       }
       else if(this.tab == 3)
       {
         this.showAboutUs = false;
         this.products = bio;
-        $scope.physCol = {"background-color" : "white"};
-        $scope.chemCol = {"background-color" : "white"};
-        $scope.bioCol = {"background-color" : "rgb(95, 218, 204)"};
-        $scope.plasCol = {"background-color" : "white"};
-        $scope.abtUs = {"background-color" : "white"};
-        $scope.contUs = {"background-color" : "white"};
-        $scope.careers = {"background-color" : "white"};
+        changeButtonToWhite(button);
+        button = bioCol;
+        changeColorButton(button);
       }
       else if(this.tab == 4)
       {
         this.showAboutUs = false;
         this.products = plasticWare;
-        $scope.physCol = {"background-color" : "white"};
-        $scope.chemCol = {"background-color" : "white"};
-        $scope.bioCol = {"background-color" : "white"};
-        $scope.plasCol = {"background-color" : "rgb(95, 218, 204)"};
-        $scope.abtUs = {"background-color" : "white"};
-        $scope.contUs = {"background-color" : "white"};
-        $scope.careers = {"background-color" : "white"};
+        changeButtonToWhite(button);
+        button = plasCol;
+        changeColorButton(button);
       }
       else if(this.tab == 5){
         this.showAboutUs = true;
         this.products = null;
-        $scope.physCol = {"background-color" : "white"};
-        $scope.chemCol = {"background-color" : "white"};
-        $scope.bioCol = {"background-color" : "white"};
-        $scope.plasCol = {"background-color" : "white"};
-        $scope.abtUs = {"background-color" : "rgb(95, 218, 204)"};
-        $scope.contUs = {"background-color" : "white"};
-        $scope.careers = {"background-color" : "white"};
+        changeButtonToWhite(button);
+        button = abtUs;
+        changeColorButton(button);
         $scope.events = [{
       badgeClass: 'info',
       badgeIconClass: 'glyphicon-check',
@@ -95,24 +94,16 @@
       else if(this.tab == 6){
         this.products = null;
         this.showAboutUs = false;
-        $scope.physCol = { "background-color" : "white"};
-        $scope.chemCol = {"background-color" : "white"};
-        $scope.bioCol = {"background-color" : "white"};
-        $scope.plasCol = {"background-color" : "white"};
-        $scope.abtUs = {"background-color" : "white"};
-        $scope.contUs = {"background-color" : "rgb(95, 218, 204)"};
-        $scope.careers = {"background-color" : "white"};
+        changeButtonToWhite(button);
+        button = contUs;
+        changeColorButton(button);
       }
       else if(this.tab == 7){
         this.showAboutUs = false;
         this.products = null;
-        $scope.physCol = {"background-color" : "white"};
-        $scope.chemCol = {"background-color" : "white"};
-        $scope.bioCol = {"background-color" : "white"};
-        $scope.plasCol = {"background-color" : "white"};
-        $scope.abtUs = {"background-color" : "white"};
-        $scope.contUs = {"background-color" : "white"};
-        $scope.careers = {"background-color" : "rgb(95, 218, 204)"};
+        changeButtonToWhite(button);
+        button = careers;
+        changeColorButton(button);
       }
     };
 
@@ -187,47 +178,6 @@
 
   });
 
- /*for search results */
-  app.controller('searchController', ['$scope',function($scope) {
-      $scope.name = "";
-      $scope.hideSearchResult = false;
-      $scope.prodSearchNames =[];
-
-      $scope.isSearchFound = function() {
-        var searchVal = document.getElementById("searchBar").value;
-        //if input value(upper or lowercase) is a substring and not empty
-        if(searchVal!=="")
-        {
-          for(var i=0; i< physics.length; i++)
-          {
-            if(physics[i].name.indexOf(searchVal)!== -1 ||
-            physics[i].name.toLowerCase().indexOf(searchVal)!== -1)
-            {
-              $scope.prodSearchNames.push(physics[i].name);
-              return $scope.hideSearchResult = true;
-          }
-            else{$scope.hideSearchResult =false;} } }
-          else{$scope.hideSearchResult = false;}
-      };
-
-      $scope.isSearchNotFound = function()
-      {
-        var searchVal = document.getElementById("searchBar").value;
-        if(searchVal!==""){
-          if($scope.prodSearchNames.length != 0){
-            for(var i=0; i< $scope.prodSearchNames.length; i++)
-            {
-              if($scope.prodSearchNames[i].indexOf(searchVal) == -1 ||
-              $scope.prodSearchNames[i].toLowerCase().indexOf(searchVal)== -1){
-                $scope.prodSearchNames.splice(i,1);
-              }
-            }
-          }
-        }
-      };
-  }]);
-
-
   app.controller('iconChanger', function($scope, $mdToast){
     this.icon = 0;
     this.heartChange = false;
@@ -258,10 +208,6 @@
       }
     };
 
-
-  });
-
-  app.controller('timelineController', function($scope){
 
   });
 

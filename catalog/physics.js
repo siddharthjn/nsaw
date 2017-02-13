95e5b5cda3637b97d7e0f4b85197cfe0eb99edd9
SUNNYJAIN17 <nsawsj@gmail.com>
2017-02-13 23:47:20 +0530

    Update physics.js

--- catalog/physics.js
+++ catalog/physics.js
@@ -1,20 +1,35 @@
 var physics =[
   { img:"image/bar.jpg", name : "Bar & Gauge Apparatus", price: "On Request", description:"For illustrating expansion by heating and contraction by cooling. Comprising a bar of iron mounted on a support rod with wooden handle, and a brass gauge with a cut-out which barely admits the bar lengthways while cold, and a hole which passes the bar endways only while cold."},
   {img:"image/ringandball.jpg",name :"Ball & Ring Apparatus",price:"On Request",description:"An apparatus for demonstrating thermal expansion. 18 mm,25mm"},
-  {img:"image/.jpg",name :"",price:"On Request",description:""},
-  {img:"image/.jpg",name :"",price:"On Request",description:""},
-  {img:"image/.jpg",name :"",price:"On Request",description:""},
-  {img:"image/.jpg",name :"",price:"On Request",description:""},
-  {img:"image/.jpg",name :"",price:"On Request",description:""},
-  
-  {img:"image/centrifugetubeconicalbottom.jpg", name:"Centrifuge Tube Conical Bottom", price: 25, description:"Moulded in Polypropylene, these tubes are clear, strong & safe. These autoclavable tubes are provided with printed graduations on the outer surface. The built in seal ring in the cap makes these tubes absolutely leakproof.", material: "Polypropylene", specs: "Can withstand forces upto 7500xG & 12000xG for 15 ml & 50 ml tubes respectively."},
+  {img:"image/ballbearing.jpg",name :"Ball Bearings",price:"On Request",description:"Chrome plated steel. Ball Bearings 2mm,3mm,5mm,8mm,12mm,20mm,25mm "},
+  {img:"image/ammeter.jpg",name :"Ammeter",price:"On Request",description:"Ammeter Single Range 0-10A"},
+  {img:"image/biotapparatus.jpg",name :" BIOT'S APPARATUS",price:"On Request",description:"A spherical conductor on insulating stand with two hollow hemi -spheres, each with insulated handle, to show that electrostatic charge resides on outer surface."},
+  {img:"image/twoway.jpg",name :"TWO WAY KNIFE SWITCH",price:"On Request",description:"Low voltage, double pole double throw,may be used as changeover switch with appropriate connections,molded base with plated spring metal contacts and screw connectors."},
+  {img:"image/flexible.jpg",name :" FLEXIBLE LEADS PVC ",price:"On Request",description:"Insulated with crocodile clip at each end which will accommodate a 4 mm plug.Length of the lead is approx.1 meters, it can be increased as per the demand"},
+  {img:"image/copperwire.jpg",name :" COPPER WIRE ",price:"On Request",description:"26 gauge, 4 oz.28 gauge, 4 oz."},
+  {img:"image/bell.jpg",name :" DEMONSTRATION ELECTRIC BELL",price:"On Request",description:"Mounted on panel,large size parts and visible wiring.Operates on 4-6 volts."},
+  {img:"image/coil.jpg",name :" INDUCTION COIL",price:"On Request",description:"Coils are wound with highly insulated copper wire impregnated with paraffin wax, input 6-8 V d.c. through two 4 mm socket terminals,with an adjustable trembler system. The spark suppression capacitor is housed in the base. A pair of pointer electrodes with insulated handles for adjustment of the spark gap are provided.  "},
+  {img:"image/dynamo.jpg",name :" DEMONSTRATION DYNAMO MODEL ",price:"On Request",description:"The motor is mounted on a base with a driving wheel.Output is through 4 mm sockets and a low voltage bulb is also provided as a simple output indicator."},
+  {img:"image/diode.jpg",name :"DIODE VALVE MOUNTED ",price:"On Request",description:"For experiments in diode characteristics.A double diode valve EB91 or equivalent is mounted on a two part plastic box 90x90x30 mm with sockets for two anodes, heater and cathode."},
+  {img:"image/vande.jpg", name:" VAN DE GRAFF GENERATOR, HAND DRIVEN", price: "On Request", description:"For production of high voltage static electricity, self-exciting, with insulating prespex stand, rubber belt, removable metal dome, mounted on wood base, with a driving wheel and driving belt.The discharge sphere is mounted on a pivoted arm with handle for adjustment and is attached to driving wheel support.With a 4mm socket at top of the dome and a 4 mm earth socket in the hand wheel support.Supplied with discharging sphere, but without accessories.", material: "", specs: ""},
   {img:"image/pcrtuberack.jpg", name:"PCR Tube Rack", price :35 , description:"This rack, as the name suggests, is used for keeping PCR tubes. Top & base of this rack are made of tough & transparent polycarbonate while the middle portion is made of polypropylene. The tube holding spaces are uniformly placed so as to facilitate the use of multipipettes. A no.of racks can be stacked together to save shelf space.", specs:"50ml", material:"Polycarbonate & polypropylene" },
+ {img:"image/.jpg",name :"",price:"On Request",description:""},
+   {img:"image/.jpg",name :"",price:"On Request",description:""},
+   {img:"image/.jpg",name :"",price:"On Request",description:""},
+   {img:"image/.jpg",name :"",price:"On Request",description:""},
+   {img:"image/.jpg",name :"",price:"On Request",description:""},
+   {img:"image/.jpg",name :"",price:"On Request",description:""},
+  
+  
+  
+  
+  
   {img:"image/mcttwinrack.jpg", name:"MCT Twin Rack", price :25 , description:"The transparent top provides an excellent view as well as a protection cover for the tubes stored. Each face of the base has a unique alpha-numeric index for easy identification of the tube.", specs:"0.5 ml & 1.5 ml", material:"Plastic"},
   { img:"image/grey.jpeg", name : "Screw Gauge", price: 20,description: "Screw Gauge is a tool to measure height"},
   { img:"image/grey.jpeg", name : "Optical Lense", price: 35,description: "A concave lense to focus"},
   
+   {img:"image/.jpg",name :"",price:"On Request",description:""},
   
-  {img:"image/.jpg",name :"",price:"On Request",description:""},
   
   
   

6d9aaca2572a23752515d71b01bd2866923323df
SUNNYJAIN17 <nsawsj@gmail.com>
2017-02-14 00:16:45 +0530

    Update physics.js

--- catalog/physics.js
+++ catalog/physics.js
@@ -12,18 +12,18 @@ var physics =[
   {img:"image/dynamo.jpg",name :" DEMONSTRATION DYNAMO MODEL ",price:"On Request",description:"The motor is mounted on a base with a driving wheel.Output is through 4 mm sockets and a low voltage bulb is also provided as a simple output indicator."},
   {img:"image/diode.jpg",name :"DIODE VALVE MOUNTED ",price:"On Request",description:"For experiments in diode characteristics.A double diode valve EB91 or equivalent is mounted on a two part plastic box 90x90x30 mm with sockets for two anodes, heater and cathode."},
   {img:"image/vande.jpg", name:" VAN DE GRAFF GENERATOR, HAND DRIVEN", price: "On Request", description:"For production of high voltage static electricity, self-exciting, with insulating prespex stand, rubber belt, removable metal dome, mounted on wood base, with a driving wheel and driving belt.The discharge sphere is mounted on a pivoted arm with handle for adjustment and is attached to driving wheel support.With a 4mm socket at top of the dome and a 4 mm earth socket in the hand wheel support.Supplied with discharging sphere, but without accessories.", material: "", specs: ""},
-  {img:"image/pcrtuberack.jpg", name:"PCR Tube Rack", price :35 , description:"This rack, as the name suggests, is used for keeping PCR tubes. Top & base of this rack are made of tough & transparent polycarbonate while the middle portion is made of polypropylene. The tube holding spaces are uniformly placed so as to facilitate the use of multipipettes. A no.of racks can be stacked together to save shelf space.", specs:"50ml", material:"Polycarbonate & polypropylene" },
- {img:"image/.jpg",name :"",price:"On Request",description:""},
+  {img:"image/young.jpg", name:"YOUNG'S MODEL APPARATUS", price :"On Request" , description:"Two metal frames are connected by a pivoted link which also carries a spirit level.A micrometer head reading to 0.01mm is provided to re-adjust the spirit level.With two self-centering wire pin chucks, a ceiling bracket also with two pin chucks, a counterpoise weight & supply of suitable wires but without slotted masses.", specs:"", material:"" },
+ {img:"image/barlowheel.jpg",name :"BARLOW WHEEL ",price:"On Request",description:"For demonstrating the conversion of electric energy into mechanical energy. Star-shaped copper disc 9 cm. Diameter rotatable vertically on pair of adjustable centers and dipping into mercury trough between the vertical limbs of a small horseshoe magnet on a wood base 15x 10 cm. Disc adjustable vertically on metal pillar which is connected to one of two terminals mounted on base.The other terminal is connected to the mercury trough.The apparatus will operates on 4 volts at which voltage it takes 1.8 amps., without battery."},
+   {img:"image/batteryhydrometer.jpg",name :"BATTERY HYDROMETER",price:"On Request",description:"Pipette type, comprising glass float calibrated having coloured zones for charged, partly charged and uncharged conditions.Compressible bulb and rubber tube with fittings to glass body."},
+   {img:"image/charles.jpg",name :" CHARLE'S LAW APPARATUS",price:"On Request",description:"Mounted on wooden stand nicely polished with heavy metal base, finished in grey colour, having levelling screws.Overall height 120 cm. Boxwood scale 100 cm, graduated in mm., reading in both directions.The mercury reservoir held in clamp slides on metal rod chromium plated.A Jolly's air bulb made form coming glass is supplied mounted on a wooden frame which can be slotted on the side."},
+   {img:"image/conductor.jpg",name :"CONDUCTOR",price:"On Request",description:"Plated brass, each conductor on insulated stand"},
+   {img:"image/cone.jpg",name :" DISSECTIBLE CONE",price:"On Request",description:"The wooden cone has four separable parts.Dissecting the cone yields cross sections with differing orientation withrespect to the axis of the cone: the circle, the ellipse, the parabola, and the hyperbola."},
+   {img:"image/rod.jpg",name :"EBONITE ROD ",price:"On Request",description:"Polished, 300 x 13 mm length x diam."},
+   {img:"image/whirl.jpg",name :" ELECTRIC WHIRL",price:"On Request",description:"On stand, with insulated base"},
+   {img:"image/expansion.jpg",name :" EXPANSION APPARATUS",price:"On Request",description:"Two cast metal uprights, height 18 cm, fitted with two nickel plated rods supporting expansion bar of length 38 cm. Expansion bar is fixed to one upright and moves a pointer on the other, when heated.With scale reading0 to 90°, supplied with one each aluminium, brass and iron expansion bars."},
    {img:"image/.jpg",name :"",price:"On Request",description:""},
    {img:"image/.jpg",name :"",price:"On Request",description:""},
    {img:"image/.jpg",name :"",price:"On Request",description:""},
-   {img:"image/.jpg",name :"",price:"On Request",description:""},
-   {img:"image/.jpg",name :"",price:"On Request",description:""},
-  
-  
-  
-  
-  
   {img:"image/mcttwinrack.jpg", name:"MCT Twin Rack", price :25 , description:"The transparent top provides an excellent view as well as a protection cover for the tubes stored. Each face of the base has a unique alpha-numeric index for easy identification of the tube.", specs:"0.5 ml & 1.5 ml", material:"Plastic"},
   { img:"image/grey.jpeg", name : "Screw Gauge", price: 20,description: "Screw Gauge is a tool to measure height"},
   { img:"image/grey.jpeg", name : "Optical Lense", price: 35,description: "A concave lense to focus"},

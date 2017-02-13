636cda7bcd406a7e1b981ea192585a44a96c9dea
SUNNYJAIN17 <nsawsj@gmail.com>
2017-02-13 22:44:54 +0530

    Update physics.js

--- catalog/physics.js
+++ catalog/physics.js
@@ -1,8 +1,21 @@
 var physics =[
-  { img:"image/centrifugetuberoundbottom.jpg", name : "Centrifuge Tube Round Bottom", price: 50, description: "Centrifuge tubes withround bottom are moulded in polypropylene & polycarbonate. These tubes have excellent clarity and are strong enough to be used upto 50,000xG. These tubes can be autoclaved, however, repeated autoclaving in PC tubes reduces their mechanical strength.", material: "Polypropylene & Polycarbonate", specs: "Available in all sizes."},
+  { img:"image/bar.jpg", name : "Bar & Gauge Apparatus", price: "On Request", description:"For illustrating expansion by heating and contraction by cooling. Comprising a bar of iron mounted on a support rod with wooden handle, and a brass gauge with a cut-out which barely admits the bar lengthways while cold, and a hole which passes the bar endways only while cold."},
+  {img:"image/ringandball.jpg",name :"Ball & Ring Apparatus",price:"On Request",description:"An apparatus for demonstrating thermal expansion. 18 mm,25mm"},
+  {img:"image/.jpg",name :"",price:"On Request",description:""},
+  {img:"image/.jpg",name :"",price:"On Request",description:""},
+  {img:"image/.jpg",name :"",price:"On Request",description:""},
+  {img:"image/.jpg",name :"",price:"On Request",description:""},
+  {img:"image/.jpg",name :"",price:"On Request",description:""},
+  
   {img:"image/centrifugetubeconicalbottom.jpg", name:"Centrifuge Tube Conical Bottom", price: 25, description:"Moulded in Polypropylene, these tubes are clear, strong & safe. These autoclavable tubes are provided with printed graduations on the outer surface. The built in seal ring in the cap makes these tubes absolutely leakproof.", material: "Polypropylene", specs: "Can withstand forces upto 7500xG & 12000xG for 15 ml & 50 ml tubes respectively."},
   {img:"image/pcrtuberack.jpg", name:"PCR Tube Rack", price :35 , description:"This rack, as the name suggests, is used for keeping PCR tubes. Top & base of this rack are made of tough & transparent polycarbonate while the middle portion is made of polypropylene. The tube holding spaces are uniformly placed so as to facilitate the use of multipipettes. A no.of racks can be stacked together to save shelf space.", specs:"50ml", material:"Polycarbonate & polypropylene" },
   {img:"image/mcttwinrack.jpg", name:"MCT Twin Rack", price :25 , description:"The transparent top provides an excellent view as well as a protection cover for the tubes stored. Each face of the base has a unique alpha-numeric index for easy identification of the tube.", specs:"0.5 ml & 1.5 ml", material:"Plastic"},
   { img:"image/grey.jpeg", name : "Screw Gauge", price: 20,description: "Screw Gauge is a tool to measure height"},
-  { img:"image/grey.jpeg", name : "Optical Lense", price: 35,description: "A concave lense to focus"}
+  { img:"image/grey.jpeg", name : "Optical Lense", price: 35,description: "A concave lense to focus"},
+  
+  
+  {img:"image/.jpg",name :"",price:"On Request",description:""},
+  
+  
+  
 ];

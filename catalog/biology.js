2ef5acc5a64932b0c54954cae57eb2a0935eab8c
SUNNYJAIN17 <nsawsj@gmail.com>
2017-02-28 22:45:31 +0530

    Update biology.js

--- catalog/biology.js
+++ catalog/biology.js
@@ -1,5 +1,17 @@
 var bio =[
-  { img:"image/singlenose.jpg", name : "Single Nose Microscope", price: "On Request", description: "Metal construction eyepiece 10x objective: 10x total magnification: 100x plane concave mirror on gamble mount for illumination gives clear images easy to operate ideal for school & kids." , Specification : " Mechanical Tube Length 160 mm Upright Eye Tube Eyepieces 10X Achromatic Objectives 10X Fine AdjustmentStage Area 85 x 90 mm Illuminator Concave - Plano Mirror 40 mm. Ø"},
-  { img:"image/grey.jpeg", name : "Heart Model", price: 20,description: "Screw Gauge is a tool to measure height"},
-  {img:"image/grey.jpeg", name : "Lungs Model", price: 35,description: "A concave lense to focus"}
+  { img:"image/singlenosemicroscope.jpg", name : "Single Nose Microscope", price: "On Request", description: "Metal construction eyepiece 10x objective: 10x total magnification: 100x plane concave mirror on gamble mount for illumination gives clear images easy to operate ideal for school & kids." , Specification : " Mechanical Tube Length 160 mm Achromatic Objectives 10X Fine AdjustmentStage Area 85 x 90 mm Illuminator Concave - Plano Mirror 40 mm. Ø"},
+  { img:"image/juniormedical.jpg", name : "JUNIOR MEDICAL  MICROSCOPE (moveable condenser)", price :"On Request",description:" Best resolution optical system microscope,Eyepieces H10x & H 15X (2Nos),Parfocal Achro-Objectives 10x, 40x/45x Retractable SL,Rack & Pinion focus system with safety stopper,Fine focus by slow motion mechanism,Plain square stage with two stage clips,Efficient NA 1.25 condenser with iris diaphragm moveable on rack & pinion.Plano-concave mirror provides transmitted light source"},
+  {img:"image/MedicalMicroscope.JPG", name:" MEDICAL / LABORATORY MICROSCOPE", price: 35,description: "High resolution optical system microscope for Educational purpose,Body inclinable to 90° on base,Eyepieces H10x & H15X (2Nos),Parfocal Achro-Objectives 10x, 40x/45xSL, 100xOil,Rack & Pinion focus system with safety stopper, Fine focus by slow motion mechanism,Plain square stage with two stage clips,Efficient NA 1.25 condenser with iris diaphragm moveable on rack & pinion.,Plano-concave mirror provides transmitted light source"},
+  /*{img:"image/  ",name:"",price:"On Request",description:" "},
+  {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},
+    {img:"image/  ",name:"",price:"On Request",description:" "},*/
+  
+
 ];

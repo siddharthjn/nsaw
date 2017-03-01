b2a3c23e34fb9f4bd47b26c303c9de4b40b336de
SUNNYJAIN17 <nsawsj@gmail.com>
2017-03-01 22:49:04 +0530

    Update chemistry.js

--- catalog/chemistry.js
+++ catalog/chemistry.js
@@ -2,5 +2,20 @@ var chemistry =[
   {img:"image/beakersEurodesign.jpg",name :"BEAKER( EURO DESIGN)", price:100, description : "EURO design Beakers, moulded in Polypropylene, too have excellent clarity and very good chemical resistance. These autoclavable Beakers also have prominently raised graduations on the outer surface.It's tapered spout is very helpful in conveniently pouring the solutions", specs:"25ml, 50ml, 100ml, 250ml, 500ml, 1000ml", material:"Polypropylene"},
   {img:"image/beakers.png",name :"BEAKERS", price: 100, description:"Beakers, moulded in Polypropylene, have excellent clarity and very good chemical resistance. These autoclavable Beakers have prominently raised graduations on the outer surface. Pouring a solution becomes very convenient because of its tapered spout.", specs:"50ml, 100ml, 250ml, 500ml, 1000ml, 2000ml", material:"Polypropylene"},
   {img:"image/burette.jpg",name :"BURETTE", price : 100, description: "This Burette has a transparent acrylic pipe and a leak proof TPX stopcock fitted with a self lubricating PTFE plug. Each Burette is individually calibrated to give maximum accuracy. Acrylic body is unaffected by dilute mineral acids & bases except Hydrofluoric acide & Ammonium Hydroxide. The pH range of 1-14 is ideal and can be used up to 5%wt/wt of acids or 30% bases.", specs:"25ml, 50ml, 100ml", material:"Polymethyl Methacrylate/TPX"},
-  {img:"image/pipettepump.jpg", name:"PIPETTE PUMP", price: 250, description:"A fast release pipetting device for precise pipetting & zip quick emptying. The knurled thumbwheel can be rotated to draw up or dispense liquids while the releaser may be pushed in for zip-quick emptying. The moulded chuck has silicon rubber collet inside to hold various pipettes of standard sizes. It resists acids & alkalies and can be easily disassembled for thorough cleaning.", specs:"2ml, 10ml ,25ml", material:"Acrylonitrile Butadiene Styrene"}
+  {img:"image/pipettepump.jpg", name:"PIPETTE PUMP", price: 250, description:"A fast release pipetting device for precise pipetting & zip quick emptying. The knurled thumbwheel can be rotated to draw up or dispense liquids while the releaser may be pushed in for zip-quick emptying. The moulded chuck has silicon rubber collet inside to hold various pipettes of standard sizes. It resists acids & alkalies and can be easily disassembled for thorough cleaning.", specs:"2ml, 10ml ,25ml", material:"Acrylonitrile Butadiene Styrene"},
+  {img:"image/tripodstand.jpg ",name:"Tripod Stand ( triangular) ",price:" On Request",description:"Top triangular, length of one side 13cm and height 20cm. "},
+ {img:"image/retortring.jpg ",name:"RETORT RING ",price:" On Request",description:"RETORT RING MADE OF MILD STEEL ROD, POWDER COATED WITH T SCREW .sizes available:2",2 1/2",3",4",5",6""},
+  {img:"image/ RETORTRINGIMPROVED.jpg",name:"RETORT RING IMPROVED ",price:"On Request ",description:" RETORT RING MADE OF MILD STEEL ROD WITH PRESSURE DAI CASTING BOSS HEAD WITH MILD STEEL SCREW,SIZES AVAILABLE:62mm,75mm,100mm"},
+  {img:"image/gastap.jpg ",name:"GAS TAP ",price:"On Request ",description:" GAS TAP, MADE OF BRASS CHROME PLATED WITH STOP COCK AND ONE SPARE NOSEL.Types:one way,two way,three way,four way."},
+/*  {img:"image/ ",name:" ",price:"On Request ",description:" "},
+  {img:"image/ ",name:" ",price:"On Request ",description:" "},
+  {img:"image/ ",name:" ",price:" On Request",description:" "},
+  {img:"image/ ",name:" ",price:" On Request",description:" "},
+  {img:"image/ ",name:" ",price:"On Request ",description:" "},
+  {img:"image/ ",name:" ",price:"On Request ",description:" "},
+  {img:"image/ ",name:" ",price:"On Request ",description:" "},
+  {img:"image/ ",name:" ",price:"On Request ",description:" "},*/
+ 
+  
+
 ];

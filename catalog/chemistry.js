00e868da8ee9f77e58f1bd0cbe488dbb262cfce5
Siddharth Jain <siddharth.smu@gmail.com>
2017-03-01 20:06:42 -0400

    Update chemistry.js

--- catalog/chemistry.js
+++ catalog/chemistry.js
@@ -4,7 +4,7 @@ var chemistry =[
   {img:"image/burette.jpg",name :"BURETTE", price : 100, description: "This Burette has a transparent acrylic pipe and a leak proof TPX stopcock fitted with a self lubricating PTFE plug. Each Burette is individually calibrated to give maximum accuracy. Acrylic body is unaffected by dilute mineral acids & bases except Hydrofluoric acide & Ammonium Hydroxide. The pH range of 1-14 is ideal and can be used up to 5%wt/wt of acids or 30% bases.", specs:"25ml, 50ml, 100ml", material:"Polymethyl Methacrylate/TPX"},
   {img:"image/pipettepump.jpg", name:"PIPETTE PUMP", price: 250, description:"A fast release pipetting device for precise pipetting & zip quick emptying. The knurled thumbwheel can be rotated to draw up or dispense liquids while the releaser may be pushed in for zip-quick emptying. The moulded chuck has silicon rubber collet inside to hold various pipettes of standard sizes. It resists acids & alkalies and can be easily disassembled for thorough cleaning.", specs:"2ml, 10ml ,25ml", material:"Acrylonitrile Butadiene Styrene"},
   {img:"image/tripodstand.jpg ",name:"Tripod Stand ( triangular) ",price:" On Request",description:"Top triangular, length of one side 13cm and height 20cm. "},
- {img:"image/retortring.jpg ",name:"RETORT RING ",price:" On Request",description:"RETORT RING MADE OF MILD STEEL ROD, POWDER COATED WITH T SCREW .sizes available:2",2 1/2",3",4",5",6""},
+ {img:"image/retortring.jpg ",name:"RETORT RING ",price:" On Request",description:"RETORT RING MADE OF MILD STEEL ROD, POWDER COATED WITH T SCREW ."},
   {img:"image/ RETORTRINGIMPROVED.jpg",name:"RETORT RING IMPROVED ",price:"On Request ",description:" RETORT RING MADE OF MILD STEEL ROD WITH PRESSURE DAI CASTING BOSS HEAD WITH MILD STEEL SCREW,SIZES AVAILABLE:62mm,75mm,100mm"},
   {img:"image/gastap.jpg ",name:"GAS TAP ",price:"On Request ",description:" GAS TAP, MADE OF BRASS CHROME PLATED WITH STOP COCK AND ONE SPARE NOSEL.Types:one way,two way,three way,four way."},
   {img:"image/ 18mmwidehoffm.jpg",name:"HOFFMAN SCREW CLIPS ",price:"On Request ",description:"HOFF MAN SCREW MADE OF BRASS CHROME PLATED.sizes available:12mm,18mm,25mm,35mm. "},
@@ -12,7 +12,7 @@ var chemistry =[
   {img:"image/filterpump.jpg ",name:"FILTER PUMP  ",price:"On Request ",description:"FILTER PUMP EDWARED TYPE, THIS MECHANISM CREATES VACUUM WHILE CONNECTING WITH WATER TAP (MADE OF EXTRUDED BRASS, CHROME PLATED) "},
   {img:"image/labjack.jpg ",name:"LABORATORY JACK ",price:"On Request ",description:"LABORATORY JACK, AN ALL PURPOSE CONSTRUCTED FROM STAINLESS STEEL, MILD STEEL. SUITABLE FOR HOT PLATES, BURNERS, BATHS, GLASSWARE & MANY MORE ITEMS.IT IS A KNOB OPERATED SCISSOR ACTION LIFTING MECHANISM FOR QUICK, EASY & ACCURATE HEIGHT ADJUSTMENT. MILD STEEL JACKS ARE FINISHED WITH POWDER COATED. size available: 12x14 cm,20x20 cm,25x25 cm."}
   {img:"image/rodfor stand.JPG ",name:" ROD FOR RETORT STAND",price:" On Request",description:"SPARE RODS FOR RETORT STAND, MADE OF MILD STEEL OR STAINLESS STEEL."},
-  {img:"image/forceps.jpg ",name:"FORCEPS ",price:" On Request",description:" MADE OF STAINLESS STEEL (BLUNT/ POINTED).size:5",6",8",10",12"},
+  {img:"image/forceps.jpg ",name:"FORCEPS ",price:" On Request",description:" MADE OF STAINLESS STEEL (BLUNT/ POINTED)"},
   /*{img:"image/ ",name:" ",price:"On Request ",description:" "},
   {img:"image/ ",name:" ",price:"On Request ",description:" "},
   {img:"image/ ",name:" ",price:"On Request ",description:" "},
